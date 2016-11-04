03518286047080206e90e1976933b3911641efc1
Shashank <Shashank@10.57.191.162>
2016-11-04 13:12:20 -0500

    new commit

---
+++ src/studentdemo/student.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentdemo;
+
+/**
+ *
+ * @author Shashank
+ */
+public class student {
+    private String firstname;
+    private String lastname;
+    private double gpa;
+    private int totalCredits;
+    
+    
+    
+    
+}
